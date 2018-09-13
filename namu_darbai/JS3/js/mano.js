58ac3fef49995531372c9dd682c20f347c4d0171
terracotta01 <terracotta22@gmail.com>
2018-09-13 14:31:52 +0300

    pridetas failas

---
+++ namu_darbai/JS3/js/mano.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  var age = prompt("Kiek jums metų?",12);
+  var atsakymas = "Jūs per jaunas užeiti į svetainę",;
+  if (age < 18) {
+    console.log("Jūs per jaunas užeiti į svetainę");
+  }
+  else {
+    console.log("Užeiti į svetainę jums galima");
+  }
+}
+
+myFunction();
