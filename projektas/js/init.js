eb90b4226aac92b3b0ffcd39380a4fb7a536cbc2
terracotta01 <terracotta22@gmail.com>
2018-09-25 23:07:48 +0300

    projektas

--- projektas/js/init.js
+++ projektas/js/init.js
@@ -6,3 +6,21 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// floating action button
+
+  $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
+
+//open modal
+  $(".myImg").click(function(){
+    $("#myModal").css({"display":"block"});
+    $("#img01").prop("src", $(this).prop("src"));
+    $("#caption").html($(this).prop("alt"));
+  });
+
+  // close image by click
+  $("#myModal").click(function(){
+    $("#myModal").css({"display":"none"});
+  });
