38369b4e9cdc0eb764ef14b15f9eac3dfddd0979
terracotta01 <terracotta22@gmail.com>
2018-10-08 11:57:14 +0300

    projektas su js sutvarkytu

--- diena07/footer.php
+++
@@ -1,5 +0,0 @@
-    </div>
-    </div>
-    <div id="footer">2018. Vilnius Coding School.</div>
-  </body>
-</html>
--- diena07/includes/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $server = 'localhost';
-  $username = 'root';
-  $password = '';
-  $database = 'forumas';
-
-  $conn = mysqli_connect($server, $username, $password, $database);
-
- ?>
